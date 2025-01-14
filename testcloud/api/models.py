1b1b35ac230edd357bc97f4e7347961967218af7
bazaram2 <medas.bazaras2@mail.dcu.ie>
2025-01-14 12:04:25 +0100

    Remodeled Models and added Users

--- testcloud/api/models.py
+++ testcloud/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
