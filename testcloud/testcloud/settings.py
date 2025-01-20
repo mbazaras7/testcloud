28d9c97a8fd92785ff8c1792b3cffd165ca7ee29
bazaram2 <medas.bazaras2@mail.dcu.ie>
2025-01-20 19:03:56 +0100

    added dependencies

--- testcloud/testcloud/settings.py
+++ testcloud/testcloud/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DUMMY_AUTH_TOKEN = "test-token" #Remove in production
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
