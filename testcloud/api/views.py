a16fe3d73f3d71f43b1a6cc64276d6fc4da73d0f
bazaram2 <medas.bazaras2@mail.dcu.ie>
2025-02-06 22:31:33 +0100

    fixed

--- testcloud/api/views.py
+++ testcloud/api/views.py
@@ -158,10 +158,8 @@ class ProcessReceiptView(APIView):
         blob_name = generate_filename(uploaded_file.name)
         
         receiptUrl = upload_image_to_azure(uploaded_file, blob_name)
-        #endpoint = os.environ["DOCUMENTINTELLIGENCE_ENDPOINT"]
-        endpoint = "https://testcloud-receipt.cognitiveservices.azure.com/"
-        #key = os.environ["DOCUMENTINTELLIGENCE_API_KEY"]
-        key = "55ZEzXXYdoiuCQykzrZFzTMUxdD4gaw3kqUx8o1U0heIaVoXxu2vJQQJ99BAACi5YpzXJ3w3AAALACOGLe2w"
+        endpoint = os.environ["DOCUMENTINTELLIGENCE_ENDPOINT"]
+        key = os.environ["DOCUMENTINTELLIGENCE_API_KEY"]
 
         document_intelligence_client = DocumentIntelligenceClient(endpoint=endpoint, credential=AzureKeyCredential(key))
         print(receiptUrl)
@@ -231,12 +229,9 @@ class ProcessReceiptView(APIView):
 def upload_image_to_azure(image_file, blob_name):
     """Uploads an image to Azure Blob Storage and returns the URL."""
     
-    #AZURE_STORAGE_ACCOUNT_NAME = os.environ["AZURE_STORAGE_ACCOUNT_NAME"]
-    AZURE_STORAGE_ACCOUNT_NAME = "testcloudblob"
-    #AZURE_STORAGE_ACCOUNT_KEY = os.environ["AZURE_STORAGE_ACCOUNT_KEY"]
-    AZURE_STORAGE_ACCOUNT_KEY = "EMNuCh/mIyM97+CH6MYiXzeXv7Vwkp2VqAXtn+jaqGfuvZf6biFl4j+4v37b3lwv8JJ0Cplq7E21+AStyNNiKg=="
-    #AZURE_CONTAINER_NAME = os.environ["AZURE_CONTAINER_NAME"]
-    AZURE_CONTAINER_NAME = "testcloud-blob"
+    AZURE_STORAGE_ACCOUNT_NAME = os.environ["AZURE_STORAGE_ACCOUNT_NAME"]
+    AZURE_STORAGE_ACCOUNT_KEY = os.environ["AZURE_STORAGE_ACCOUNT_KEY"]
+    AZURE_CONTAINER_NAME = os.environ["AZURE_CONTAINER_NAME"]
 
     compressed_image = compress_image(image_file)
     # Connect to Azure Blob Storage
